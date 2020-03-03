8ae5b11e381d8e451f6871c56ab1cd119aea7606
Maxime Cloutier <maxime.cloutier3@gmail.com>
2020-03-03 17:05:31 -0500

    Add new components

---
+++ Assets/Components/Brightness.cs
@@ -0,0 +1,6 @@
+﻿using Unity.Entities;
+
+public struct Brightness : IComponentData 
+{
+    public float Value;
+}
---
+++ Assets/Components/Size.cs
@@ -0,0 +1,6 @@
+﻿using Unity.Entities;
+
+public struct Size : IComponentData
+{
+    public float Value;
+}
