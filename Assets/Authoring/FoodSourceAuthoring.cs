11abc08d7e43611438918068af4f6052e5d5cceb
zephon82 <jeanjulientuffreau@hotmail.com>
2020-03-04 10:52:50 -0500

    FoodSource/Colony Prefab/Authoring

---
+++ Assets/Authoring/FoodSourceAuthoring.cs
@@ -0,0 +1,25 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequiresEntityConversion]
+public class FoodSourceAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        // TODO: Get real value from mesh...
+        float sphereRadius = 0.5f;
+
+        dstManager.AddComponent<TagFoodSource>(entity);
+        dstManager.AddComponent<Position>(entity);
+        dstManager.AddComponentData(entity, new Radius
+        {
+            Value = transform.localScale.magnitude * sphereRadius
+        });
+
+        dstManager.RemoveComponent<Translation>(entity);
+        dstManager.RemoveComponent<Rotation>(entity);
+    }
+}
