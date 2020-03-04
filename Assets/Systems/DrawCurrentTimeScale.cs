f3c9b6050666ba7443688a07321fb060319b0371
wpare <wpare@eidosmontreal.com>
2020-03-04 14:53:39 -0500

    Add new timescale system + debug draw

---
+++ Assets/Systems/DrawCurrentTimeScale.cs
@@ -0,0 +1,28 @@
+﻿using Unity.Entities;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrawCurrentTimeScale : MonoBehaviour
+{
+    Text text;
+    static bool showText = true;
+    OverrideWorldTimeSystem timeSystem;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+        text.enabled = showText;
+        timeSystem = World.DefaultGameObjectInjectionWorld.GetExistingSystem<OverrideWorldTimeSystem>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            showText = !showText;
+            text.enabled = showText;
+        }
+
+        text.text = "Current Time Scale: " + timeSystem.timeScale.ToString();
+    }
+}
---
+++ Assets/Systems/OverrideWorldTimeSystem.cs
@@ -0,0 +1,30 @@
+﻿using Unity.Core;
+using Unity.Entities;
+using Unity.Jobs;
+using UnityEngine;
+
+[UpdateInGroup(typeof(InitializationSystemGroup))]
+[UpdateAfter(typeof(UpdateWorldTimeSystem))]
+public class OverrideWorldTimeSystem : JobComponentSystem
+{
+    public TimeData time;
+    public int timeScale = 1;
+    int multiplier = 2;
+
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+        if (Input.inputString == "1")
+        { timeScale = 1; }
+
+        else if (Input.inputString == "0")
+        { timeScale = 0; }
+
+        else if (Input.inputString != "" && int.TryParse(Input.inputString, out int tempTimeScale) && tempTimeScale != 0)
+        { timeScale = tempTimeScale * multiplier; };
+
+        time = new TimeData(World.Time.ElapsedTime, World.Time.DeltaTime * timeScale);
+        World.SetTime(time);
+
+        return inputDependencies;
+    }
+}
