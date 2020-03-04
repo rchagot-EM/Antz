02f4443e4ea81a6ab947531057c5bf1ad39c7e23
zephon82 <jeanjulientuffreau@hotmail.com>
2020-03-04 09:53:02 -0500

    Obstacle Prefab

---
+++ Assets/Authoring/ObstacleAuthoring.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+
+
+[DisallowMultipleComponent]
+[RequiresEntityConversion]
+public class ObstacleAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+
+    }
+}
