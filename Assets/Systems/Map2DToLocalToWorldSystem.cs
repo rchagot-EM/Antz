0476d6af9979bc46c0300e4da8d8f788122a197c
rchagot <>
2020-03-04 16:55:12 -0500

    Support FacingAngle

--- Assets/Systems/Map2DToLocalToWorldSystem.cs
+++ Assets/Systems/Map2DToLocalToWorldSystem.cs
@@ -7,20 +7,41 @@ using Unity.Transforms;
 
 
 [UpdateInGroup(typeof(TransformSystemGroup))]
-public class Pos2DToLocalToWorldSystem : JobComponentSystem
+public class Map2DToLocalToWorldSystem : JobComponentSystem
 {
-    EntityQuery m_Query;
+    //EntityQuery m_Pos2Query;
+    //EntityQuery m_Pos2AngleQuery;
 
 
     [BurstCompile]
+    [ExcludeComponent(typeof(FacingAngle))]
     struct Pos2DToLocalToWorldSystemJob : IJobForEach<LocalToWorld, Position, NonUniformScale>
     {
-        public float rcpMapSize;
-             
+        public float rcpMapSize;   
         
-        public void Execute(ref LocalToWorld matrix44, [ReadOnly] ref Position position, [ReadOnly] ref NonUniformScale nonUniformScale)
+        public void Execute(ref LocalToWorld matrix44,
+            [ReadOnly] ref Position position,
+            [ReadOnly] ref NonUniformScale nonUniformScale)
         {
             var rotation = quaternion.identity;
+            var scale = float4x4.Scale(nonUniformScale.Value);
+            var translation = new float3(position.Value * rcpMapSize, 0f);
+            matrix44.Value = math.mul(new float4x4(rotation, translation), scale);
+        }
+    }
+
+
+    [BurstCompile]
+    struct Pos2DAngleToLocalToWorldSystemJob : IJobForEach<LocalToWorld, Position, FacingAngle, NonUniformScale>
+    {
+        public float rcpMapSize;
+        
+        public void Execute(ref LocalToWorld matrix44,
+            [ReadOnly] ref Position position,
+            [ReadOnly] ref FacingAngle facingAngle,
+            [ReadOnly] ref NonUniformScale nonUniformScale)
+        {
+            var rotation = quaternion.Euler(0f, 0f, facingAngle.Value);
             var scale = float4x4.Scale(nonUniformScale.Value);
             var translation = new float3(position.Value * rcpMapSize, 0f);
             matrix44.Value = math.mul(new float4x4(rotation, translation), scale);
@@ -32,14 +53,25 @@ public class Pos2DToLocalToWorldSystem : JobComponentSystem
     {
         base.OnCreate();
 
-        m_Query = GetEntityQuery(new EntityQueryDesc
+        /*m_Pos2Query = GetEntityQuery(new EntityQueryDesc
         {
             All = new[] {
                 ComponentType.ReadWrite<LocalToWorld>(),
                 ComponentType.ReadOnly<Position>(),
                 ComponentType.ReadOnly<NonUniformScale>() },
+            None = new[] { ComponentType.ReadOnly<FacingAngle>() },
             Options = EntityQueryOptions.FilterWriteGroup
         });
+
+        m_Pos2AngleQuery = GetEntityQuery(new EntityQueryDesc
+        {
+            All = new[] {
+                ComponentType.ReadWrite<LocalToWorld>(),
+                ComponentType.ReadOnly<Position>(),
+                ComponentType.ReadOnly<FacingAngle>(),
+                ComponentType.ReadOnly<NonUniformScale>() },
+            Options = EntityQueryOptions.FilterWriteGroup
+        });*/
 
         RequireSingletonForUpdate<AntManagerSettings>();
     }
@@ -48,10 +80,24 @@ public class Pos2DToLocalToWorldSystem : JobComponentSystem
     {
         var settings = GetSingleton<AntManagerSettings>();
 
-        var job = new Pos2DToLocalToWorldSystemJob();
-        job.rcpMapSize = 1.0f / settings.MapSize;
+        var job1 = new Pos2DToLocalToWorldSystemJob
+        {
+            rcpMapSize = 1.0f / settings.MapSize
+        };
+        var jobHandle1 = job1.Schedule(this, inputDependencies);
 
-        // Now that the job is set up, schedule it to be run. 
-        return job.Schedule(this, inputDependencies);
+        var job2 = new Pos2DAngleToLocalToWorldSystemJob
+        {
+            rcpMapSize = 1.0f / settings.MapSize
+        };
+#if false
+        var jobHandle2 = job2.Schedule(this, inputDependencies);
+
+        return JobHandle.CombineDependencies(jobHandle1, jobHandle2);
+#else
+        var jobHandle2 = job2.Schedule(this, jobHandle1);
+
+        return jobHandle2;
+#endif
     }
 }
