41aec15431d3578a2dd4019606014d930d167bef
rchagot <>
2020-03-04 09:33:29 -0500

    Authoring folder

---
+++ Assets/Authoring/AntManagerAuthoring.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+
+
+[DisallowMultipleComponent]
+[RequiresEntityConversion]
+public class AntManagerAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+
+    }
+}
