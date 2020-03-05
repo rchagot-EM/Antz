1fc1ca0ba0d86bb177f772d8fa3dde1c577892e1
Maxime Cloutier <maxime.cloutier3@gmail.com>
2020-03-05 10:48:51 -0500

    Clean ant authoring

--- Assets/Authoring/AntAuthoring.cs
+++ Assets/Authoring/AntAuthoring.cs
@@ -10,22 +10,11 @@ public class AntAuthoring : MonoBehaviour, IConvertGameObjectToEntity
 
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
     {
-        dstManager.AddComponentData(entity, new Speed
-        {
-            Value = 0f
-        });
-
-        //TODO 0,0?
-        dstManager.AddComponentData(entity, new Position
-        {
-            Value = new float2(0f, 0f)
-        });
-
+        dstManager.AddComponent<Speed>(entity);
+        dstManager.AddComponent<Position>(entity);
         dstManager.AddComponentData(entity, new Brightness { Value = 1.0f });
-
         dstManager.RemoveComponent<Translation>(entity);
         dstManager.RemoveComponent<Rotation>(entity);
-
         dstManager.AddComponent<TagAnt>(entity);
         dstManager.AddComponent<FacingAngle>(entity);
         dstManager.AddComponent<PheromoneSteering>(entity);
