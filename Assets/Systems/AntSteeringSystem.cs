749046a5888c663826c0a8b1422b7a9d282010d1
wpare <wpare@eidosmontreal.com>
2020-03-05 11:01:55 -0500

    Change random namespace usage

--- Assets/Systems/AntSteeringSystem.cs
+++ Assets/Systems/AntSteeringSystem.cs
@@ -2,23 +2,27 @@
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
-using UnityEngine;
+using Unity.Mathematics;
 
 public class AntSteeringSystem : JobComponentSystem
 {
+    Random RandomGen;
+
     [BurstCompile]
     struct RandomSteeringJob : IJobForEach_C<RandomSteering>
     {
         public float RandomSteeringRange;
+        public Random random;
 
         public void Execute(ref RandomSteering randomSteering)
         {
-            randomSteering.Value = Random.Range(-RandomSteeringRange, RandomSteeringRange);
+            randomSteering.Value = random.NextFloat(-RandomSteeringRange, RandomSteeringRange);
         }
     }
 
     protected override void OnCreate()
     {
+        RandomGen = new Random(123456);
         base.OnCreate();
         RequireSingletonForUpdate<AntManagerSettings>();
     }
@@ -30,6 +34,7 @@ public class AntSteeringSystem : JobComponentSystem
 
         var job = new RandomSteeringJob
         {
+            random = RandomGen,
             RandomSteeringRange = settings.RandomSteering
         };
 
